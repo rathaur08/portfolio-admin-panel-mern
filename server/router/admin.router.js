ff9c9ca78dfb989342b097b449cfda02a2ec205c
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-07-10 15:59:57 +0530

    Update Code and Add Functionalty | Secure our Admin Route with JWT Verification and Middleware Integration

--- server/router/admin.router.js
+++ server/router/admin.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as adminController from "../controllers/admin.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/users").get(adminController.getAllUsers);
-router.route("/contacts").get(adminController.getAllContacts);
+router.route("/users").get(authMiddleware, adminController.getAllUsers);
+router.route("/contacts").get(authMiddleware, adminController.getAllContacts);
 
 
 export const adminRoute = router;
