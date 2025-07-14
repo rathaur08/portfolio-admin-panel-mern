2d8fc21db2476c4ca9ddea5c06fd66b2ab80103c
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-07-14 17:41:32 +0530

    Update Code and Add Functionalty || Admin CRUD: Updating Users Data | Backend Route & Logics

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -31,6 +31,70 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+
+// getUserById
+export const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await User.findOne({ _id: id }, { password: 0 })
+
+
+    if (!data || data.length === 0) {
+      return res.status(404).json({
+        status: "404",
+        message: "No Users Found",
+      });
+    }
+
+    return res.status(200).json({
+      status: "200",
+      message: "Users data",
+      data: data,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      status: "500",
+      message: "Internal Server Error",
+    });
+    console.error(error);
+    next(error);
+  }
+};
+
+
+// updateUserById
+export const updateUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const Updateddata = await User.updateOne({ _id: id }, {
+      $set: data,
+    })
+
+    if (!Updateddata || Updateddata.length === 0) {
+      return res.status(404).json({
+        status: "404",
+        message: "No Users Found",
+      });
+    }
+
+    return res.status(200).json({
+      status: "200",
+      message: "Users data",
+      data: Updateddata,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      status: "500",
+      message: "Internal Server Error",
+    });
+    console.error(error);
+    next(error);
+  }
+};
+
 // deleteUserById
 
 export const deleteUserById = async (req, res) => {
--- server/router/admin.router.js
+++ server/router/admin.router.js
@@ -7,6 +7,10 @@ const router = Router();
 
 router.route("/users").get(authMiddleware, adminMiddleware, adminController.getAllUsers);
 
+router.route("/users/:id").get(authMiddleware, adminMiddleware, adminController.getUserById);
+
+router.route("/users/update/:id").patch(authMiddleware, adminMiddleware, adminController.updateUserById);
+
 router.route("/users/delete/:id").delete(authMiddleware, adminMiddleware, adminController.deleteUserById);
 
 router.route("/contacts").get(authMiddleware, adminController.getAllContacts);
